098e49ba80ad8a1cc30c4ec9f07a076a4167df0a
Samiyuru Senarathne <samiyuru@gmail.com>
2014-03-31 23:17:23 +0530

    dbmodles

---
+++ app/dbmodles.js
@@ -0,0 +1,10 @@
+/**
+ * Created by samiyuru on 3/31/14.
+ */
+
+exports.dbmodles = new function(){
+
+    var mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/test');
+
+}
