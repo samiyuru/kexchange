a659112462ec572e3747203f7c834594bb9a88d9
Samiyuru Senarathne <samiyuru@gmail.com>
2014-03-31 22:54:20 +0530

    rout tests

--- app/app.js
+++ app/app.js
@@ -6,8 +6,11 @@ var app = express();
 app.set('port', 3000);
 app.use(express.logger('dev'));
 app.use(express.methodOverride());
-app.use(express.static('public'));//map static files routes. files needed to render index.html
+app.use(express.static('./public'));//map static files routes. files needed to render index.html
+app.use(app.routes);
 
+
+require('./routes').routes(app);
 
 
 http.createServer(app).listen(app.get('port'), function(){
