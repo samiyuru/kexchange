cf739e2066549890a31cb513e6607eed87cd85e7
Samiyuru Senarathne <samiyuru@gmail.com>
2014-04-02 22:13:59 +0530

    db models

--- app/dbmodles.js
+++ app/dbmodles.js
@@ -6,10 +6,22 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/kexchange');
 
 var Profile = mongoose.model('profile', {
-    nickname: String,
-    name: String,
-    wealth: Number,
-    propic: String
+    nickname: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    wealth: {
+        type: String
+    },
+    propic: {
+        type: String,
+        required: true
+    }
 });
 
 
@@ -17,33 +29,88 @@ exports.createProfile = function (profile, cb) {
     Profile.create(profile, cb);
 };
 
-exports.getProfile = function (id , cb) {
+exports.getProfile = function (id, cb) {
 
 };
 
 exports.getProfiles = function (opt, cb) {
     /*
-    * {  skip:0,  limit:0 }
-    * */
-    if(opt == null){
+     * {  skip:0,  limit:0 }
+     * */
+    if (opt == null) {
 
-    }else{
+    } else {
 
     }
- };
+};
 
 
-
-
-
+/*
+* if auction qty, minbid and exp are mandatory
+* */
 var Product = mongoose.model('product', {
-    title:String,
-    detail:String,
-    type:Number,
-    qty:Number,
-    isAuction:Boolean,
-    minBid:Number,
-    price:Number,
-    expire:Date,
-    imgs:[String]
+    title: {
+        type: String,
+        required: true
+    },
+    detail: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.ObjectId,
+        required: true
+    },
+    type: {
+        type: Number,
+        required: true
+    },
+    qty: Number,
+    remQty: Number,
+    isAuction: {
+        type: Boolean,
+        required: true
+    },
+    minBid: Number,
+    price: Number,
+    expire: Date,
+    imgs: [String],
+    bids: [
+        {
+            person: {
+                type: mongoose.Schema.ObjectId,
+                required: true
+            },
+            date: {
+                type: Date,
+                required: true
+            },
+            bid: {
+                type: Number,
+                required: true
+            }
+        }
+    ]
 });
+
+exports.createProduct = function (owner, product, cb) {
+    Profile.create(profile, cb);
+};
+
+exports.placeBid = function(productID, person, bid, cb){
+
+};
+
+exports.purchase = function(productID, person, cb){
+
+};
+
+exports.getProducts = function(opt, cb){
+/*
+* {
+*   skip:0,
+*   limit:0,
+*   isAuction:false
+* }
+* */
+};
